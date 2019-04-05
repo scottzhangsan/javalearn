6f8abf03cc30dcfff99f014a6c585637449a5000
scottzhangsan <15837192793@163.com>
2019-04-05 09:34:01 +0800

    init project

---
+++ src/main/java/com/java/learn/SpringbootApplicationDemo.java
@@ -0,0 +1,12 @@
+package com.java.learn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationDemo {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
